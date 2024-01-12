5f8943808c32a8b9a32defb73bc63e6eed8ecdc1
Torean Joel <toreanjoel@Toreans-MacBook-Pro.local>
2024-01-12 02:04:30 +0200

    feat: add ability to delete servers, reinit server on crash with old id, remove deleted from registry, delete non needed code

--- lib/wsserve/servers/subserver.ex
+++ lib/wsserve/servers/subserver.ex
@@ -10,22 +10,33 @@ defmodule Wsserve.Servers.Subserver do
   require Logger
 
   def start_link(args \\ %{}) do
-    uuid = UUID.uuid4()
+    # Generate a default UUID
+    default_uuid = UUID.uuid4()
+
+    # Extract the id from custom_state if it exists and is not empty
+    id =
+      args
+      |> Map.get(:custom_state, %{})
+      |> Map.get(:config, %{})
+      |> Map.get(:id, default_uuid)
+
+    Logger.info("starting with id: #{id}")
 
     GenServer.start_link(
       __MODULE__,
-      Map.merge(args, %{
-        id: uuid
-      }),
-      name: String.to_atom(Atom.to_string(__MODULE__) <> ":" <> uuid)
+      Map.merge(args, %{id: id}),
+      name: String.to_atom(Atom.to_string(__MODULE__) <> ":" <> id)
     )
   end
 
   def init(args) do
+    Logger.info("args init")
+    Logger.info(inspect(args))
+
     config = %Wsserve.Servers.Subserver.Config{
       pid: self(),
       manager_pid: args.manager_pid,
-      id: Map.get(args, :id, UUID.uuid4())
+      id: Map.get(args, :id)
     }
 
     # we set the deafault state of allowing a lobby for all servers
@@ -73,17 +84,10 @@ defmodule Wsserve.Servers.Subserver do
     end
   end
 
-  # TODO: The updating of the config wont be needed for now
-  # Update the config property. Will replace what is currently there if it already exists
-  # def handle_call({:update_config, key, value}, _from, state) do
-  #   updated_config = Map.put(state.config, key, value)
-  #   updated_state = Map.put(state, :config, updated_config)
-  #   {:reply, updated_state, updated_state}
-  # end
-
   # Sync: Update the state of the specific channel - will merge to the data currenly existing in memory
   def handle_call({:update_channel, channel, data}, _from, state) do
     curr_channel = Map.get(state.channel_states, channel, false)
+
     if curr_channel do
       type = curr_channel.type
       updated_state = update_channel(channel, data, state, type)
@@ -107,13 +111,6 @@ defmodule Wsserve.Servers.Subserver do
     {:reply, {:ok, "Channel #{channel_name} created"}, new_state}
   end
 
-  # TODO: Think about if this is reelant
-  # Async: Update the state of the specific channel - will merge to the data currenly existing in memory
-  # def handle_cast({:update_channel, channel, data}, state) do
-  #   updated_state = update_channel(channel, data, state)
-  #   {:noreply, updated_state}
-  # end
-
   # here we listen for the requests to send configs to process to manage
   def handle_info({:request_config, caller_pid}, state) do
     send(caller_pid, {:server_config, state.config})
@@ -121,28 +118,29 @@ defmodule Wsserve.Servers.Subserver do
   end
 
   # Here we update channel data to the state
-  def update_channel(channel, data, %{channel_states: channel_states} = state, type)
-    when type in [:accumulative, :collaborative] do
-  curr_channel = Map.get(channel_states, channel, %{})
+  defp update_channel(channel, data, %{channel_states: channel_states} = state, type)
+       when type in [:accumulative, :collaborative] do
+    curr_channel = Map.get(channel_states, channel, %{})
 
-  updated_details =
-    case type do
-      :accumulative ->
-        payload = Map.new() |> Map.put(DateTime.utc_now() |> DateTime.to_unix(), data)
-        Map.merge(curr_channel.state, payload)
+    updated_details =
+      case type do
+        :accumulative ->
+          payload = Map.new() |> Map.put(DateTime.utc_now() |> DateTime.to_unix(), data)
+          Map.merge(curr_channel.state, payload)
 
-      :collaborative ->
-        # Assuming data is a map with user ID as key and payload as value
-        payload = Map.new() |> Map.put(data.user.id, data)
-        Map.merge(curr_channel.state, payload)
+        :collaborative ->
+          # Assuming data is a map with user ID as key and payload as value
+          payload = Map.new() |> Map.put(data.user.id, data)
+          Map.merge(curr_channel.state, payload)
 
-      _ ->
-        Logger.error("Unknown channel type: #{inspect(type)}")
-        curr_channel.state
-    end
+        _ ->
+          Logger.error("Unknown channel type: #{inspect(type)}")
+          curr_channel.state
+      end
 
-  updated_channel_states = Map.put(channel_states, channel, %{curr_channel | state: updated_details})
-  Map.put(state, :channel_states, updated_channel_states)
-end
+    updated_channel_states =
+      Map.put(channel_states, channel, %{curr_channel | state: updated_details})
 
+    Map.put(state, :channel_states, updated_channel_states)
+  end
 end
--- lib/wsserve/servers/subserver_manager.ex
+++ lib/wsserve/servers/subserver_manager.ex
@@ -44,9 +44,30 @@ defmodule Wsserve.Servers.SubserverManager do
   end
 
   # delete a sub server
+  def handle_call({:delete_server, server_id}, _from, state) do
+    # we get the process by id
+    pid = Map.get(state.servers, server_id, false)
+
+    if pid do
+      case DynamicSupervisor.terminate_child(SubserverSupervisor, pid) do
+        :ok ->
+          updated_servers_state = remove_from_registry(state, server_id)
+          {:reply, updated_servers_state, updated_servers_state}
+
+        _ ->
+          Logger.error(
+            "Unable to delete process. There was a probllem removing subserver isntance"
+          )
+
+          {:reply, state, state}
+      end
+    else
+      Logger.error("Unable to delete process, not found in registry")
+      {:reply, state, state}
+    end
+  end
 
   # remove reference to process and pid
-
   def handle_info({:server_config, %{id: server_id, pid: server_pid}}, state) do
     # we start monitoring the process for other messages
     # we get a ref that when things crash we need to use to cleanup manager state
@@ -58,36 +79,41 @@ defmodule Wsserve.Servers.SubserverManager do
     {:noreply, updated_state}
   end
 
-  # handling other massages that we dont know about yet
+  # Subserver was shut down
+  def handle_info({:DOWN, _ref, _, _, reason}, state) when reason === :shutdown do
+    Logger.info("Subserver was shut down")
+    # Here we should remove but we need to store process refs in order to track pid when using callback monitoring
+    {:noreply, state}
+  end
+
+  # Subserver dies or crashes for some reason
   def handle_info({:DOWN, _ref, _, _, data}, state) do
     {_, [{_, _, [_, _, prev_process_state], _}, _, _, _]} = data
     {status, _} = request_create_server(prev_process_state)
+
     case status do
       :ok ->
         Logger.info("Successfully revived crashed server data with last known data")
         # Delete the ref in the state of the crashed process from the manager
         {:noreply, remove_from_registry(state, Kernel.get_in(prev_process_state, [:config, :id]))}
+
       _ ->
         Logger.error("There was a problem creating the server (recreate from falling)")
         {:noreply, state}
     end
   end
 
-  # Catch all at the end for mssages
-  # handling other massages that we dont know about yet
-  # TODO: Look at managing the killing or deleting a server
+  # unhanled process message here
   def handle_info(event, state) do
-    IO.inspect("Non handled process messages")
-    IO.inspect("-----")
-    IO.inspect(event)
+    Logger.info("Unhandled process message")
+    Logger.info("Event: \n #{inspect(event)}")
     {:noreply, state}
   end
 
   # init function that if this were to go down and back up it initialized with running servers
   defp init_running_servers do
     servers = DynamicSupervisor.which_children(SubserverSupervisor)
-    IO.inspect("init_running: Check dynamic supervisor")
-    IO.inspect(servers)
+
     if !Enum.empty?(servers) do
       Enum.each(servers, fn {_, server_pid, _, _} ->
         # request that the servers tell this manager their config details
